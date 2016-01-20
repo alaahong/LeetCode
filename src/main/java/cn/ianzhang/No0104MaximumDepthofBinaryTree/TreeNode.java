a92932b44b267719e926289e3fc514e643dbd7f7
ian.zhang <ian.zhangzhe@gmail.com>
2016-01-20 23:50:31 +0800

    No0104MaximumDepthofBinaryTree

---
+++ src/main/java/cn/ianzhang/No0104MaximumDepthofBinaryTree/TreeNode.java
@@ -0,0 +1,14 @@
+package cn.ianzhang.No0104MaximumDepthofBinaryTree;
+
+/**
+ * Created by Ian on 2016/1/20.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
---
+++ src/test/java/cn/ianzhang/No0104MaximumDepthofBinaryTree/SolutionTest.java
@@ -0,0 +1,34 @@
+package cn.ianzhang.No0104MaximumDepthofBinaryTree;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Ian on 2016/1/20.
+ */
+public class SolutionTest {
+    @Test
+    public void testMaxDepth() throws Exception {
+        TreeNode root = new TreeNode(1);
+        root.left = generateOneHand(true, 10);
+        root.left = generateOneHand(false, 15);
+        Solution s = new Solution();
+        assertEquals(s.maxDepth(root), 16);
+    }
+
+    private TreeNode generateOneHand(boolean leftHand, int depth) {
+        TreeNode tree = new TreeNode(1);
+        TreeNode resultTree = tree;
+        for (int i = 0; i < depth - 1; i++) {
+            if (leftHand) {
+                tree.left = new TreeNode(1);
+                tree = tree.left;
+            } else {
+                tree.right = new TreeNode(1);
+                tree = tree.right;
+            }
+        }
+        return resultTree;
+    }
+}
