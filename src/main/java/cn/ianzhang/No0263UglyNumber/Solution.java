9d93627115c4331605771c060d46ac79be91e6cc
ian.zhang <ian.zhangzhe@gmail.com>
2016-01-28 23:22:29 +0800

    No0263UglyNumber

--- src/main/java/cn/ianzhang/No0263UglyNumber/Solution.java
+++ src/main/java/cn/ianzhang/No0263UglyNumber/Solution.java
@@ -5,19 +5,10 @@ package cn.ianzhang.No0263UglyNumber;
  */
 public class Solution {
     public boolean isUgly(int num) {
-        int n = num;
-        if(n <= 0){
-            return false;
-        }
-        while (n % 2 == 0) {
-            n = n / 2;
-        }
-        while (n % 3 == 0) {
-            n = n / 3;
-        }
-        while (n % 5 == 0) {
-            n = n / 5;
-        }
-        return n == 1 || n == 2 || n == 3 || n == 5;
+        if (num <= 0) return false;
+        while (num % 2 == 0) num /= 2;
+        while (num % 3 == 0) num /= 3;
+        while (num % 5 == 0) num /= 5;
+        return num == 1;
     }
 }
