490501cba5272286ac3b95b6b486900e56a38bdb
Ian <ian.zhangzhe@gmail.com>
2020-05-17 19:38:41 +0800

    No0021MergeTwoLists Updated

--- src/main/java/cn/ianzhang/No0021MergeTwoLists/Solution.java
+++ src/main/java/cn/ianzhang/No0021MergeTwoLists/Solution.java
@@ -1,8 +1,5 @@
 package cn.ianzhang.No0021MergeTwoLists;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @Author: Ian
  * @Date: 2020/5/17 19:01
@@ -10,34 +7,24 @@ import java.util.List;
  */
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode lastNode = null;
-        List<Integer> list= new ArrayList<>();
-        while(l1 != null && l2 != null){
-            if(l1.val < l2.val){
-                list.add(l1.val);
+        ListNode head = new ListNode(-1);
+        ListNode curNode = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curNode.next = l1;
                 l1 = l1.next;
-            }else{
-                list.add(l2.val);
+            } else {
+                curNode.next = l2;
                 l2 = l2.next;
             }
+            curNode = curNode.next;
         }
-        if(l1 == null && l2 != null){
-            while(l2 != null){
-                list.add(l2.val);
-                l2 = l2.next;
-            }
+        if (l1 == null) {
+            curNode.next = l2;
         }
-        if(l1 != null && l2 == null){
-            while(l1 != null){
-                list.add(l1.val);
-                l1 = l1.next;
-            }
+        if (l2 == null) {
+            curNode.next = l1;
         }
-        for (int i = list.size()-1; i >=0 ; i--) {
-            ListNode curListNode = new ListNode(list.get(i));
-            curListNode.next = lastNode;
-            lastNode = curListNode;
-        }
-        return lastNode;
+        return head.next;
     }
 }
